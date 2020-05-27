4cfe5f6a5041a1eba8de03bc4d63d3871ffd3f5a
atharv79 <55636611+atharv79@users.noreply.github.com>
2020-05-27 10:21:48 +0530

    Create Assignment 10.2.py

---
+++ Python Data Structures/Week 6/Assignment 10.2.py
@@ -0,0 +1,25 @@
+'''
+10.2 Write a program to read through the mbox-short.txt and figure out
+the distribution by hour of the day for each of the messages.
+You can pull the hour out from the 'From ' line by finding the time and then
+splitting the string a second time using a colon.
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+Once you have accumulated the counts for each hour, print out the counts,
+sorted by hour as shown below.
+'''
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+counts = dict()
+for line in handle:
+    if not line.startswith('From '): continue
+    #print(line)
+    lin = line.rstrip().split()
+    time = lin[5]
+    tmesp = time.split(':')
+    hour = tmesp[0]
+    #print(hour)        
+    counts[hour] = counts.get(hour, 0) + 1
+x = sorted(counts.items())
+for k,v in x:
+    print(k, v)
